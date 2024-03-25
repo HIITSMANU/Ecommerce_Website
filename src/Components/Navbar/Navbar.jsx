1e6e0877e258fa3479a313872491df5fb809da31
HIITSMANU <132491372+HIITSMANU@users.noreply.github.com>
2024-03-25 12:25:15 +0530

    navbar_updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,19 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState,useEffect } from "react";
 import "./Navbar.css"
 import logo from "../Assets/logo.png"
 import cart_icon from "../Assets/cart_icon.png"
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../Context/Context";
 const Navbar = () =>{
-    const {cartItem} = useContext(ShopContext)
+    const { cartItem } = useContext(ShopContext);
+    const [cartCount, setCartCount] = useState(0);
+
+    useEffect(() => {
+        // Calculate total number of items in the cart
+        const totalCount = Object.values(cartItem).reduce((acc, quantity) => acc + quantity, 0);
+        setCartCount(totalCount);
+    }, [cartItem]);
+
 
 
     const[menu,setMenu] = useState("shop")
@@ -24,7 +32,9 @@ const Navbar = () =>{
             <div className="nav_login_cart">
                  <Link to='/loginsignup'><button>Login</button></Link>
                  <Link to='/cart'><img src={cart_icon} alt="carts" /></Link>
-                <div className="nav_cart_count"></div>
+                 <div className="nav_cart_icon">
+                        {cartCount > 0 && <div className="nav_cart_count">{cartCount}</div>}
+                    </div>
             </div>
         </div>
     )
